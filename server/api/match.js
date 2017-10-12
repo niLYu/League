2d1a5d3b8f6959d2667ea1186892eda5dc4fcc1c
Jack Jiang <thejackjiang@gmail.com>
2017-10-12 10:18:30 -0400

    Add: champion, player, match models added

--- server/api/match.js
+++ server/api/match.js
@@ -6,8 +6,10 @@ const secrets = require('../../secrets')
 
 //gets live game by summoner name
 router.get('/match/:name', (req, res, next) => {
+  console.log('hit me')
   request(`https://na1.api.riotgames.com/lol/summoner/v3/summoners/by-name/${req.params.name}?api_key=${secrets.LEAGUE_API_KEY}`, function (error, response, body) {
     let playerInfo = JSON.parse(body)
+    console.log(playerInfo)
     request(`https://na1.api.riotgames.com/lol/spectator/v3/active-games/by-summoner/${playerInfo.id}?api_key=${secrets.LEAGUE_API_KEY}`, function (error, response, body) {
       let matchInfo = JSON.parse(body)
       res.json(matchInfo)
