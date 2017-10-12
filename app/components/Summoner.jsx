a392184cca0f25f0dcd310a8cdcfe68501469aaf
Yu Lin <yulin89@gmail.com>
2017-10-12 12:34:25 -0400

    Add: User reducer to handle frontend request to get user data

--- app/components/Summoner.jsx
+++ app/components/Summoner.jsx
@@ -1,6 +1,16 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import axios from 'axios'
+
+import { fetchUser } from '../reducers/user'
 
 class Summoner extends Component {
+  componentDidMount() {
+    const search = this.props.location.search
+    const params = new URLSearchParams(search)
+    const username = params.get('username')
+    this.props.fetchUser(username)
+  }
   render() {
     return (
       <div>Summoner</div>
@@ -8,4 +18,8 @@ class Summoner extends Component {
   }
 }
 
-export default Summoner;
+const mapStateToProps = ({ user }) => ({ user })
+
+const mapDispatchToProps = { fetchUser }
+
+export default connect(mapStateToProps, mapDispatchToProps)(Summoner);
--- app/reducers/index.jsx
+++ app/reducers/index.jsx
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
 
-const rootReducer = combineReducers({
+import user from './user';
 
+const rootReducer = combineReducers({
+  user
 })
 
 export default rootReducer;
