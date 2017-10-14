0162106f573e15e5af16032a0a161084d2fe7232
Yu Lin <yulin89@gmail.com>
2017-10-14 01:21:12 -0400

    Fix: CSS Homepage horizontal overflow

--- app/components/Home.jsx
+++ app/components/Home.jsx
@@ -4,12 +4,9 @@ import styled from 'styled-components';
 
 const Container = styled.div`
   display: flex;
+  height: 100vh;
   align-items: center;
   justify-content: center;
-  margin: auto;
-  height: 80%;
-  width: 100%;
-  position: absolute;
 `;
 
 const Form = styled.form`
