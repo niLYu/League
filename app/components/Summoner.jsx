f44d949b5c45185dcba3a4c998406b941111ee3c
Yu Lin <yulin89@gmail.com>
2017-10-13 22:29:25 -0400

    Style: Remove dead code

--- app/components/Summoner.jsx
+++ app/components/Summoner.jsx
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import axios from 'axios'
 
 import { fetchUser } from '../reducers/user'
 
--- app/reducers/user.js
+++ app/reducers/user.js
@@ -4,7 +4,7 @@ const GET_USER = 'GET_USER';
 
 const getUser = user => ({ type: GET_USER, user})
 
-export const fetchUser = (username) => dispatch => {
+export const fetchUser = username => dispatch => {
   axios.get(`/api/playerInfo/player/${username}`)
   .then(res => res.data)
   .then(user => dispatch(getUser(user)))
