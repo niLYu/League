8c163d47eae81b5356a6853f91cec95ebd4f874a
Yu Lin <yulin89@gmail.com>
2017-10-17 14:48:39 -0400

    Feat: Homepage icon/link in the navbar

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,18 +1,22 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import styled from 'styled-components';
 
 const NavContainer = styled.div`
   display: flex;
   background-color: teal;
-  margin-bottom: 20px;
+  margin-bottom: 50px;
+  justify-content: center;
+  align-items: center;
+  height: 10vh;
 `;
 
 const Navbar = () => (
-  <NavContainer>
-    <ul>
-      <li>League</li>
-    </ul>
-  </NavContainer>
+  <NavContainer >
+    <NavLink to="/" className="homeNavIcon">
+      <img id="homeIcon" src="/images/HomeIcon.png" alt="Could not load" />
+    </NavLink>
+  </NavContainer >
 );
 
 export default Navbar;
--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
     path: __dirname,
     filename: './public/bundle.js',
   },
-  devtool: 'cheap-module-eval-source-map',
+  devtool: 'eval-source-map',
   resolve: {
     extensions: ['.js', '.jsx', '.json', '*'],
   },
