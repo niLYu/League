403b3f789d0024138116f1f0409c7385b76a9ae1
Yu Lin <yulin89@gmail.com>
2017-10-18 23:31:22 -0400

    Add: League of legends current news scraping route

---
+++ server/api/riotScraper.js
@@ -0,0 +1,29 @@
+const router = require('express').Router();
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+router.get('/news', (req, res) => {
+  const url = 'http://na.leagueoflegends.com';
+
+  axios.get(`${url}/en/news`).then((response) => {
+    const $ = cheerio.load(response.data);
+    const newsInfo = [];
+    let counter = 0;
+
+    $('.gs-container').filter(function () {
+      const news = {};
+      const data = $(this);
+      if (counter < 5) {
+        news.title = $('.lol-core-file-formatter', data)[0].attribs.title;
+        news.content = $('.teaser-content', data).children().text();
+        news.url = `${url}${$('.lol-core-file-formatter', data)[0].attribs.href}`;
+        news.imageUrl = `${url}${$('.file-image', data).children()[0].attribs.src}`;
+        newsInfo.push(news);
+      }
+      counter += 1;
+    });
+    res.json(newsInfo);
+  });
+});
+
+module.exports = router;
