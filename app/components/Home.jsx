515b4f20847e15e51215d0bdb5a57777eac1b1d1
Yu Lin <yulin89@gmail.com>
2017-10-26 11:14:00 -0400

    Refactor: Styled Components have been removed.  The associated CSS has been converted to CSS module format

--- app/components/Home.jsx
+++ app/components/Home.jsx
@@ -1,66 +1,8 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import styled from 'styled-components';
 
 import Carousel from './Carousel';
 import styles from './Home.css';
-
-const Container = styled.div`
-  display: flex;
-  height: 100%;
-  align-items: center;
-  justify-content: center;
-`;
-
-const Form = styled.form`
-  display: flex;
-  flex: 0.33;
-  color: black;
-  border-radius: 10%;
-  border-style: outset;
-  border-width: 1em;
-  border-color: teal;
-  align-items: center;
-  justify-content: space-between;
-
-  @media (max-width: 600px) {
-    flex: 0.65
-  }
-  @media (max-width: 400px) {
-    flex: 0.85
-  }
-`;
-
-const Label = styled.label`
-  flex: 1;
-`;
-
-const Input = styled.input`
-  font-size: 1em;
-  border-style: none;
-  margin-left 10px;
-  width: 100%;
-  &:focus ${Input} {
-    outline: none;
-  }
-`;
-const ButtonWrap = styled.div`
-  background-color: teal;
-  border-radius: 50%;
-  text-align: center;
-`;
-
-const StyledButton = styled.button`
-  background-color: teal;
-  border-radius: 50%;
-  height: 50px;
-  width: 50px;
-  font-size: 13px;
-  text-align: center;
-  &:hover ${StyledButton} {
-    background-color: white;
-  }
-`;
 
 class Home extends Component {
   constructor() {
@@ -78,27 +20,28 @@ class Home extends Component {
   render() {
     return (
       <div>
-        <Container>
-          <Form>
-            <Label>
-              <Input
+        <div className={styles.container}>
+          <form className={styles.form}>
+            <label className={styles.label} htmlFor="Summoner's name">
+              <input
+                className={styles.input}
                 type="text"
                 placeholder="Summoner's name"
                 value={this.state.playerName}
                 onChange={this.handleChange}
               />
-            </Label>
-            <ButtonWrap>
+            </label>
+            <div className={styles.button_wrapper}>
               <Link to={{
             pathname: '/summoner',
             search: `username=${this.state.playerName}`,
           }}
               >
-                <StyledButton>GO</StyledButton>
+                <button className={styles.go_button}>GO</button>
               </Link>
-            </ButtonWrap>
-          </Form>
-        </Container>
+            </div>
+          </form>
+        </div>
         <Carousel />
       </div>
     );
