d7fecb2fa61ccde6595a197e70ffdfb321b41dc5
Yu Lin <yulin89@gmail.com>
2017-11-02 13:46:19 -0400

    Feat: SummonerTabs conditional rendering implementation

--- app/components/SummonerTabs.jsx
+++ app/components/SummonerTabs.jsx
@@ -1,20 +1,54 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Leagues from './Leagues';
+import Champions from './Champions';
+import Masteries from './Masteries';
+import Runes from './Runes';
 
-const SummonerTabs = () => (
-  <div>
+class SummonerTabs extends Component {
+  constructor() {
+    super();
+    this.initialState = {
+      Leagues: false,
+      Champions: false,
+      Masteries: false,
+      Runes: false,
+    };
+    this.state = this.initialState;
+  }
+
+
+  button = name => (
     <div>
-      <button>Leagues</button>
+      <button onClick={() => this.handleClick(name)}>{name}</button>
     </div>
-    <div>
-      <button>Champions</button>
-    </div>
-    <div>
-      <button>Masteries</button>
-    </div>
-    <div>
-      <button>Runes</button>
-    </div>
-  </div>
-);
+  )
+
+  handleClick = (component) => {
+    const newState = Object.assign({}, this.state);
+    newState[component] = true;
+    this.setState(newState);
+  }
+
+  render() {
+    return (
+      <div>
+        <div>
+          <div>
+            {this.button('Leagues')}
+            {this.button('Champions')}
+            {this.button('Masteries')}
+            {this.button('Runes')}
+          </div>
+        </div>
+        <div>
+          {this.state.Leagues && <Leagues />}
+          {this.state.Champions && <Champions />}
+          {this.state.Masteries && <Masteries />}
+          {this.state.Runes && <Runes />}
+        </div>
+      </div>
+    );
+  }
+}
 
 export default SummonerTabs;
