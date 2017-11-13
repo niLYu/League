4937e251701d78faff4aefa740e893c6a997b22c
Yu Lin <yulin89@gmail.com>
2017-11-12 20:47:32 -0500

    feat: (match) render timestamp for games

--- app/components/Match.jsx
+++ app/components/Match.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import champList from '../../seed';
 
-const Match = props => (
-  <div>
-    <div>{props.champion}</div>
-  </div>
-);
+const Match = (props) => {
+  const champ = champList.Heroes[props.champion].key;
+  const datePlayed = JSON.stringify(new Date(props.timestamp));
+  return (
+    <div>
+      <h3>{datePlayed}</h3>
+      <img src={`/images/champions/${champ}.png`} alt={`${champ} icon`} />
+    </div>
+  );
+};
+
+Match.propTypes = {
+  champion: PropTypes.number.isRequired,
+  timestamp: PropTypes.number.isRequired,
+};
 
 export default Match;
--- app/components/Summoner.jsx
+++ app/components/Summoner.jsx
@@ -1,5 +1,5 @@
-import PropTypes from 'prop-types';
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import BasicProfile from './BasicProfile';
 import SummonerTabs from './SummonerTabs';
