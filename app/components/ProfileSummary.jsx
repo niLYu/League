d1a40f82f8b4c2bda73ac9f54e008cab3df71d70
Jack Jiang <thejackjiang@gmail.com>
2017-11-14 11:59:46 -0500

    feat: added icon and styling for unranked queues

--- app/components/ProfileSummary.jsx
+++ app/components/ProfileSummary.jsx
@@ -22,16 +22,24 @@ const ProfileSummary = (props) => {
             <img className={styles.rankImage}src={`/images/${soloRank[0].tier.toLowerCase()}.png`} alt="league tier" />
           </div>
           <div className={styles.flexItem}>
-            <h3>{soloRank[0].tier}  {soloRank[0].rank}</h3>
-            <h4>W/L: {soloRank[0].wins}W/{soloRank[0].losses}L </h4>
-            <h4>W/L Ratio: {(soloRank[0].wins / (soloRank[0].wins + soloRank[0].losses)) * 100}%</h4>
-            <h4>League Points: {soloRank[0].leaguePoints}</h4>
-            <h4>{soloRank[0].leagueName}</h4>
+            <h4>{soloRank[0].tier}  {soloRank[0].rank}</h4>
+            <h5>Solo Queue 5v5</h5>
+            <h5>W/L: {soloRank[0].wins}W/{soloRank[0].losses}L </h5>
+            <h5>W/L Ratio: {((soloRank[0].wins / (soloRank[0].wins + soloRank[0].losses)) * 100).toFixed(2)}%</h5>
+            <h5>League Points: {soloRank[0].leaguePoints}</h5>
+            <h5>{soloRank[0].leagueName}</h5>
           </div>
         </div>
         :
-        <div>
-        Not ranked in solo 5v5
+        <div className={styles.flexcontainer}>
+          <div className={styles.flexRankIcon}>
+            <img className={styles.rankImage}src="/images/unranked.png" alt="league tier" />
+          </div>
+          <div className={styles.flexItemCentered}>
+            <h5>Unranked</h5>
+            <h5>Solo Queue</h5>
+            <h5>5v5</h5>
+          </div>
         </div>
           }
       {flexRank.length ?
@@ -40,16 +48,24 @@ const ProfileSummary = (props) => {
             <img className={styles.rankImage}src={`/images/${flexRank[0].tier.toLowerCase()}.png`} alt="league tier" />
           </div>
           <div className={styles.flexItem}>
-            <h3>{flexRank[0].tier}  {flexRank[0].rank}</h3>
-            <h4>W/L: {flexRank[0].wins}W/{flexRank[0].losses}L </h4>
-            <h4>W/L Ratio: {((flexRank[0].wins / (flexRank[0].wins + flexRank[0].losses)) * 100).toFixed(2)}%</h4>
-            <h4>League Points: {flexRank[0].leaguePoints}</h4>
-            <h4>{flexRank[0].leagueName}</h4>
+            <h4>{flexRank[0].tier}  {flexRank[0].rank}</h4>
+            <h5>Flex Queue 5v5</h5>
+            <h5>W/L: {flexRank[0].wins}W/{flexRank[0].losses}L </h5>
+            <h5>W/L Ratio: {((flexRank[0].wins / (flexRank[0].wins + flexRank[0].losses)) * 100).toFixed(2)}%</h5>
+            <h5>League Points: {flexRank[0].leaguePoints}</h5>
+            <h5>{flexRank[0].leagueName}</h5>
           </div>
         </div>
         :
-        <div>
-        Not ranked in flex 5v5
+        <div className={styles.flexcontainer}>
+          <div className={styles.flexRankIcon}>
+            <img className={styles.rankImage}src="/images/unranked.png" alt="league tier" />
+          </div>
+          <div className={styles.flexItemCentered}>
+            <h5>Unranked</h5>
+            <h5>Flex Queue</h5>
+            <h5>5v5</h5>
+          </div>
         </div>
           }
       {ttRank.length ?
@@ -58,20 +74,26 @@ const ProfileSummary = (props) => {
             <img className={styles.rankImage}src={`/images/${ttRank[0].tier.toLowerCase()}.png`} alt="league tier" />
           </div>
           <div className={styles.flexItem}>
-            <h3>{ttRank[0].tier}  {ttRank[0].rank}</h3>
-            <h4>W/L: {ttRank[0].wins}W/{ttRank[0].losses}L </h4>
-            <h4>W/L Ratio: {((ttRank[0].wins / (ttRank[0].wins + ttRank[0].losses)) * 100).toFixed(2)}%</h4>
-            <h4>League Points: {ttRank[0].leaguePoints}</h4>
-            <h4>{ttRank[0].leagueName}</h4>
+            <h4>{ttRank[0].tier}  {ttRank[0].rank}</h4>
+            <h5>Flex Queue 3v3</h5>
+            <h5>W/L: {ttRank[0].wins}W/{ttRank[0].losses}L </h5>
+            <h5>W/L Ratio: {((ttRank[0].wins / (ttRank[0].wins + ttRank[0].losses)) * 100).toFixed(2)}%</h5>
+            <h5>League Points: {ttRank[0].leaguePoints}</h5>
+            <h5>{ttRank[0].leagueName}</h5>
           </div>
         </div>
       :
-        <div>
-      Not ranked in 3v3
+        <div className={styles.flexcontainer}>
+          <div className={styles.flexRankIcon}>
+            <img className={styles.rankImage}src="/images/unranked.png" alt="league tier" />
+          </div>
+          <div className={styles.flexItemCentered}>
+            <h5>Unranked</h5>
+            <h5>Flex Queue</h5>
+            <h5>3v3</h5>
+          </div>
         </div>
           }
-
-
     </div>
   );
 };
--- app/components/index.jsx
+++ app/components/index.jsx
@@ -5,10 +5,6 @@ export { default as ChampionMasteries } from './ChampionMasteries';
 export { default as Footer } from './Footer';
 export { default as Leaderboards } from './Leaderboards';
 export { default as LiveGame } from './LiveGame';
-<<<<<<< HEAD
-// export { default as LiveMatch } from './LiveMatch';
-=======
->>>>>>> f9067819f0c92592b2bda6f73e4fce4cfcaa1e6d
 export { default as Navbar } from './Navbar';
 export { default as NotFound } from './NotFound';
 export { default as Summoner } from './Summoner';
