3d0d27db10b2e58a36516ee2187f3f932f83b189
kyusulamita <sulamitamorales@yahoo.com>
2017-11-16 02:07:53 -0500

    feature: add deploy script

--- server/api/match.js
+++ server/api/match.js
@@ -1,20 +1,21 @@
 const router = require('express').Router();
 const axios = require('axios');
-const secrets = require('../../secrets');
+if (process.env.NODE_ENV !== 'production') require('../../secrets');
+const LEAGUE_API_KEY = process.env.LEAGUE_API_KEY;
 const Promise = require('bluebird');
 
 const apiRoute = 'https://na1.api.riotgames.com/lol/match/v3';
-const apiValidation = `?api_key=${secrets.LEAGUE_API_KEY}`;
+const apiValidation = `?api_key=${LEAGUE_API_KEY}`;
 
 // gets matches by summonerId, optional championId and season
 router.get('/summoner/:summonerId', (req, res, next) => {
   const { summonerId } = req.params;
-  let requestPath = `${apiRoute}/matchlists/by-account/${summonerId}?`;
+  let requestPath = `${apiRoute}/matchlists/by-account/${summonerId}${apiValidation}`;
   // allow for optional parameters -> season andOr champion
   const { championId, seasonId } = req.query;
   if (championId) requestPath += `champion=${championId}&`;
   if (seasonId) requestPath += `season=${seasonId}&`;
-  requestPath += `api_key=${secrets.LEAGUE_API_KEY}`;
+  // requestPath += `api_key=${LEAGUE_API_KEY}`;
   axios.get(requestPath)
     .then((championSeasonData) => { // grabs a matchList matching parameters
       let { matches } = championSeasonData.data;
--- server/index.js
+++ server/index.js
@@ -1,9 +1,10 @@
 const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
-
 const app = express();
 const db = require('../db');
+
+const PORT = process.env.PORT || 3000
 
 module.exports = app
   .use(bodyParser.urlencoded({ extended: true }))
@@ -21,6 +22,6 @@ db.sync({
 
 if (module === require.main) {
   app.listen(3000, () => {
-    console.log('App is listening on port 3000!');
+    console.log(`App is listening on port ${PORT}!`);
   });
 }
