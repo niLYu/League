7bddaaa6f1ef8113654ab2b8c30d025283f79580
Yu Lin <yulin89@gmail.com>
2017-12-11 22:41:03 -0500

    feat: (matches) display all champions present in matches

--- app/components/Matches/Players.jsx
+++ app/components/Matches/Players.jsx
@@ -1,19 +1,37 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import champList from '../../../seed';
+import styles from '../../styles/Match.css';
 
 const Players = (props) => {
+  const champs = champList.Heroes;
   const blueTeam = props.players.participantIdentities.slice(0, 5);
   const redTeam = props.players.participantIdentities.slice(5, 10);
-  console.log(blueTeam);
+  const participants = props.players.participants;
+
   return (
     <div>
       <div>
-        {blueTeam.map(member =>
-          <div>{member.player.summonerName}</div>)}
+        {blueTeam.map((member) => {
+          const champ = champs[participants[member.participantId - 1].championId].key;
+          return (
+            <div key={member.player.summonerName}>
+              <div>{member.player.summonerName}</div>
+              <img className={styles.champ_list} src={`/images/champions/${champ}.png`} alt={`${champ} icon`} />
+            </div>
+          );
+})}
       </div>
       <div>
-        {redTeam.map(member =>
-          <div>{member.player.summonerName}</div>)}
+        {redTeam.map((member) => {
+        const champ = champs[participants[member.participantId - 1].championId].key;
+        return (
+          <div key={member.player.summonerName}>
+            <div>{member.player.summonerName}</div>
+            <img className={styles.champ_list} src={`/images/champions/${champ}.png`} alt={`${champ} icon`} />
+          </div>
+        );
+})}
       </div>
     </div>
   );
